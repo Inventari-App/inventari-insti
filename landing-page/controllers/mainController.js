5d4e678c57f48193677072cf75e8b7d9883e32f0
albertrf147 <albert@netenders.com>
2023-11-07 18:09:55 +0100

    fix - landing, fixed link to app

--- landing-page/controllers/mainController.js
+++ landing-page/controllers/mainController.js
@@ -8,5 +8,5 @@ const isProduction = process.env.NODE_ENV === "production"
 
 exports.index = (req, res) => {
   // Assuming you already have the view code in views/index.ejs
-  res.render('index', { registerCenterTarget: isProduction ? "https://app.controlamaterial.com" : "http://localhost:3000" });
+  res.render('index', { appTargetUrl: isProduction ? "https://app.controlamaterial.com" : "http://localhost:3000" });
 };
