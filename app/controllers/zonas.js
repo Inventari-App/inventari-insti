a804b2fbb3fbb5adcdffc4ba42dfdccf532b3d0f
albertrf147 <albert@netenders.com>
2024-04-11 15:15:52 +0200

    standarized zonas forms

--- app/controllers/zonas.js
+++ app/controllers/zonas.js
@@ -16,13 +16,14 @@ module.exports.createZona = createItem(Zona, 'zona')
 
 module.exports.showZona = async (req, res, next) => {
   try {
+    const user = req.user
     const zona = await Zona.findById(req.params.id).populate("responsable");
 
     if (!zona) {
       req.flash("error", "No es pot trobar la zona!");
       return res.redirect("/zonas");
     }
-    res.render("zonas/show", { zona });
+    res.render("zonas/show", { zona, isAdmin: user.isAdmin });
   } catch (err) {
     next(err);
   }
