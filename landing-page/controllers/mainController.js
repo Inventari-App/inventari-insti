dab8f9012153de7b3622bd490f7a4440f7ce8853
albertrf147 <albert@netenders.com>
2024-04-11 16:12:32 +0200

    blank landing page with only register form

--- landing-page/controllers/mainController.js
+++ landing-page/controllers/mainController.js
@@ -2,8 +2,5 @@ const appTargetUrl = require("../helpers/getAppUrl")
 
 exports.index = (req, res) => {
   // Assuming you already have the view code in views/index.ejs
-  // res.render('index', { appTargetUrl: appTargetUrl(), redirect: appTargetUrl("landing") });
-
-  // Redirecting to app for now
-  res.redirect(appTargetUrl())
+  res.render('indexBlank', { appTargetUrl: appTargetUrl(), redirect: appTargetUrl("landing") });
 };
