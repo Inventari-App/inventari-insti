39046e169da1ee76dfbca59df29fd07bc38df952
albertrf147 <albert@netenders.com>
2024-04-22 09:01:30 +0200

    redirect landing to app

--- landing-page/controllers/mainController.js
+++ landing-page/controllers/mainController.js
@@ -2,5 +2,8 @@ const appTargetUrl = require("../helpers/getAppUrl")
 
 exports.index = (req, res) => {
   // Assuming you already have the view code in views/index.ejs
-  res.render('indexBlank', { appTargetUrl: appTargetUrl(), redirect: appTargetUrl("landing") });
+  // res.render('index', { appTargetUrl: appTargetUrl(), redirect: appTargetUrl("landing") });
+
+  // Redirecting to app for now
+  res.redirect(appTargetUrl())
 };
