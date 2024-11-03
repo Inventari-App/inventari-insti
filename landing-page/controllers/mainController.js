69b5d3962c20a4538036ba43869784fe4b9c26f5
albertrf147 <albert@netenders.com>
2024-11-03 18:36:28 +0100

    feat: Add login route and render login view in mainController

--- landing-page/controllers/mainController.js
+++ landing-page/controllers/mainController.js
@@ -2,8 +2,12 @@ const appTargetUrl = require("../helpers/getAppUrl")
 
 exports.index = (req, res) => {
   // Assuming you already have the view code in views/index.ejs
-  // res.render('index', { appTargetUrl: appTargetUrl(), redirect: appTargetUrl("landing") });
+  res.render('index', { appTargetUrl: appTargetUrl(), redirect: appTargetUrl("landing") });
 
   // Redirecting to app for now
-  res.redirect(appTargetUrl())
+  // res.redirect(appTargetUrl())
+};
+
+exports.login = (req, res) => {
+  res.render('login', { appTargetUrl: appTargetUrl(), redirect: "/" });
 };
